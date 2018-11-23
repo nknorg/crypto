c9737a60d588ac5eb8116dd6f8d2591b4372c114
NoelBright <noel.n.bright@gmail.com>
2018-11-23 19:36:37 +0800

    refactoring crypto module
    Signed-off-by: NoelBright <noel.n.bright@gmail.com>

--- encode.go
+++ encode.go
@@ -1,7 +1,6 @@
 package crypto
 
 import (
-	"crypto/ecdsa"
 	"crypto/elliptic"
 	"crypto/rand"
 	"errors"
@@ -300,20 +299,4 @@ func (e *PubKey) EncodePoint(isCommpressed bool) ([]byte, error) {
 	}
 
 	return encodedData, nil
-}
-
-func NewPubKey(priKey []byte) *PubKey {
-	privateKey := new(ecdsa.PrivateKey)
-	privateKey.PublicKey.Curve = algSet.Curve
-
-	k := new(big.Int)
-	k.SetBytes(priKey)
-	privateKey.D = k
-
-	privateKey.PublicKey.X, privateKey.PublicKey.Y = algSet.Curve.ScalarBaseMult(k.Bytes())
-
-	pubKey := new(PubKey)
-	pubKey.X = privateKey.PublicKey.X
-	pubKey.Y = privateKey.PublicKey.Y
-	return pubKey
 }
--- signature.go
+++ signature.go
@@ -8,35 +8,14 @@ const (
 )
 
 var (
-	sigAlgoes = make(map[SIGALGO]func() Keypair, 0)
+	sigAlgoes = make(map[SIGALGO]func() signature.Keypair, 0)
 )
 
-type PubKey interface {
-	EqualTo(pk PubKey) bool
-	//VerifyAddress(addr string) error
-	//ToAddress() string
-	Verify(data []byte, signature []byte) error
-	Marshal() ([]byte, error)
-	Unmarshal(data []byte) error
-}
-
-type PrivKey interface {
-	//PubKey() PubKey
-	Sign(data []byte) ([]byte, error)
-	Marshal() ([]byte, error)
-	Unmarshal(data []byte) error
-}
-
-type Keypair struct {
-	pk PubKey
-	sk PrivKey
-}
-
-func (sig SIGALGO) New() Keypair {
+func (sig SIGALGO) New() *signature.Keypair {
 	return sigAlgoes[sig]()
 }
 
 func init() {
-	sigAlgoes[P256R1] = NewP256R1
-	sigAlgoes[ED25519] = NewED25519
+	sigAlgoes[P256R1] = signature.NewP256R1
+	sigAlgoes[ED25519] = signature.NewED25519
 }
